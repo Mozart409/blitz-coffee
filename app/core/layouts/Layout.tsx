acc3c5583329d3c32fa660e280f6ffe44e5065ee
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-11-19 17:51:59 +0100

    :fix: :bug: fixed navbar and footer loading

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,13 +1,13 @@
-import { ReactNode, Suspense } from "react"
-import { Head, dynamic } from "blitz"
+import { ReactNode, Suspense } from "react";
+import { Head, dynamic } from "blitz";
 
 type LayoutProps = {
-  title?: string
-  children: ReactNode
-}
+  title?: string;
+  children: ReactNode;
+};
 
-const DynamicNavBar = dynamic(() => import("../components/NavBar"))
-const DynamicFooter = dynamic(() => import("../components/Footer"))
+const DynamicNavBar = dynamic(() => import("../components/NavBar"));
+const DynamicFooter = dynamic(() => import("../components/Footer"));
 
 const Layout = ({ title, children }: LayoutProps) => {
   return (
@@ -16,17 +16,15 @@ const Layout = ({ title, children }: LayoutProps) => {
         <title>{title || "Consumpation.Coffee"}</title>
       </Head>
 
+      <DynamicNavBar />
       <Suspense fallback={<div>Loading...</div>}>
-        <DynamicNavBar />
-
         <main className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="mx-auto max-w-5xl">{children}</div>
         </main>
-
-        <DynamicFooter />
       </Suspense>
+      <DynamicFooter />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
