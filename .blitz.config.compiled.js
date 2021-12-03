ed0b04e0240aa9b693d6dc7b3fa8063f9526b21a
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-12-03 22:50:44 +0100

    ci: :construction_worker: added relative CI

--- .blitz.config.compiled.js
+++ .blitz.config.compiled.js
@@ -3,10 +3,30 @@ var {sessionMiddleware, simpleRolesIsAuthorized} = require("blitz");
 var withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
+var {StatsWriterPlugin} = require("webpack-stats-plugin");
+var {RelativeCiAgentWebpackPlugin} = require("@relative-ci/agent");
 module.exports = {
   middleware: [
     sessionMiddleware({
       isAuthorized: simpleRolesIsAuthorized
     })
-  ]
+  ],
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    if (!dev && !isServer) {
+      config.plugins.push(new RelativeCiAgentWebpackPlugin({
+        stats: {excludeAssets: [/stats.json/]}
+      }));
+    }
+    config.plugins.push(new StatsWriterPlugin({
+      filename: "stats.json",
+      stats: {
+        context: "./",
+        assets: true,
+        entrypoints: true,
+        chunks: true,
+        modules: true
+      }
+    }));
+    return config;
+  }
 };
--- blitz.config.js
+++ blitz.config.js
@@ -3,18 +3,41 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 
+const { StatsWriterPlugin } = require("webpack-stats-plugin")
+const { RelativeCiAgentWebpackPlugin } = require("@relative-ci/agent")
+
 module.exports = {
   middleware: [
     sessionMiddleware({
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
-  /* Uncomment this to customize the webpack config
+
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     // Important: return the modified config
+
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new RelativeCiAgentWebpackPlugin({
+          stats: { excludeAssets: [/stats.json/] },
+        })
+      )
+    }
+    config.plugins.push(
+      new StatsWriterPlugin({
+        filename: "stats.json",
+        stats: {
+          context: "./", // optional, will improve readability of the paths
+          assets: true,
+          entrypoints: true,
+          chunks: true,
+          modules: true,
+        },
+      })
+    )
+
     return config
   },
-  */
 }
