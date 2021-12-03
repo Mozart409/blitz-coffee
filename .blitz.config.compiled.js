3c8a27ce0d81a374ee188cfe59ab0e36ffdfa7e2
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-12-03 23:14:00 +0100

    build: :building_construction: added webpack bundle analyzer

--- .blitz.config.compiled.js
+++ .blitz.config.compiled.js
@@ -1,32 +1,36 @@
 // blitz.config.js
 var {sessionMiddleware, simpleRolesIsAuthorized} = require("blitz");
+var withPlugins = require("next-compose-plugins");
 var withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
 var {StatsWriterPlugin} = require("webpack-stats-plugin");
 var {RelativeCiAgentWebpackPlugin} = require("@relative-ci/agent");
-module.exports = {
-  middleware: [
-    sessionMiddleware({
-      isAuthorized: simpleRolesIsAuthorized
-    })
-  ],
-  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
-    if (!dev && !isServer) {
-      config.plugins.push(new RelativeCiAgentWebpackPlugin({
-        stats: {excludeAssets: [/stats.json/]}
-      }));
-    }
-    config.plugins.push(new StatsWriterPlugin({
-      filename: "stats.json",
-      stats: {
-        context: "./",
-        assets: true,
-        entrypoints: true,
-        chunks: true,
-        modules: true
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  {
+    middleware: [
+      sessionMiddleware({
+        isAuthorized: simpleRolesIsAuthorized
+      })
+    ],
+    webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+      if (!dev && !isServer) {
+        config.plugins.push(new RelativeCiAgentWebpackPlugin({
+          stats: {excludeAssets: [/stats.json/]}
+        }));
       }
-    }));
-    return config;
+      config.plugins.push(new StatsWriterPlugin({
+        filename: "stats.json",
+        stats: {
+          context: "./",
+          assets: true,
+          entrypoints: true,
+          chunks: true,
+          modules: true
+        }
+      }));
+      return config;
+    }
   }
-};
+]);
